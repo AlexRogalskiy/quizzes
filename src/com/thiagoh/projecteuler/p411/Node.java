6c464137a4352e9e026f15aa38d67534bb39d59a
thiagoh <thiagoh@gmail.com>
2015-02-28 13:27:02 -0300

    project creation

---
+++ src/com/thiagoh/projecteuler/p411/Node.java
@@ -0,0 +1,8 @@
+package com.thiagoh.projecteuler.p411;
+
+public class Node {
+
+	double distanceToNN;
+	int x;
+	int y;
+}
---
+++ src/com/thiagoh/projecteuler/p411/Solution.java
@@ -0,0 +1,8 @@
+package com.thiagoh.projecteuler.p411;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+	}
+}
