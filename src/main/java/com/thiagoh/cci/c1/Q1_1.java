f2e9ad0585201ccefb928801e17c647c799aaa5e
thiagoh <thiagoh@gmail.com>
2015-03-14 18:00:17 -0300

    java question

--- src/main/java/com/thiagoh/cci/c1/Q1_1.java
+++ src/main/java/com/thiagoh/cci/c1/Q1_1.java
@@ -3,17 +3,13 @@ package com.thiagoh.cci.c1;
 public class Q1_1 {
 
 	public static boolean unique(String s) {
-
-		if (s == null) {
+		if (s == null || s.length() <= 1) {
 			return true;
 		}
-
 		boolean[] b = new boolean[100];
 		int aPos = (int) 'a';
-
 		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			int p = ((int) c) - aPos;
+			int p = ((int) s.charAt(i)) - aPos;
 			if (b[p])
 				return false;
 			b[p] = true;
