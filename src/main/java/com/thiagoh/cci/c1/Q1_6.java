8e392b4490d7a8a2481daf4b159fa0b376d80e49
thiagoh <thiagoh@gmail.com>
2015-03-17 22:30:07 -0300

    removing unnecessary lines

--- src/main/java/com/thiagoh/cci/c1/Q1_6.java
+++ src/main/java/com/thiagoh/cci/c1/Q1_6.java
@@ -6,20 +6,15 @@ public class Q1_6 {
 		if (a == null || a.length < 2) {
 			return;
 		}
-		int D = 0;
-		double N = a.length;
-		int W = (int) (N - 1);
-		while (D < Math.floor(N / 2.0)) {
-			int x = 0;
-			while (x + D < W - D) {
+		int W = a.length - 1;
+		for (int D = 0; D < Math.floor(a.length / 2.0); D++) {
+			for (int x = 0; x + D < W - D; x++) {
 				int tmp = a[D + x][D];
 				a[D + x][D] = a[W - D][x + D];
 				a[W - D][x + D] = a[W - x - D][W - D];
 				a[W - x - D][W - D] = a[D][W - x - D];
 				a[D][W - x - D] = tmp;
-				x++;
 			}
-			D++;
 		}
 	}
 }
