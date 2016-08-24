5953b5a807ddf332444ecf97835ff6ab16881051
Thiago Andrade <thiagoh@gmail.com>
2016-08-24 01:28:22 -0400

    question 10 and 4

--- src/main/python/com/thiagoh/projecteuler/Q10.py
+++ src/main/python/com/thiagoh/projecteuler/Q10.py
@@ -3,9 +3,28 @@ from math import *;
 
 # http://math.stackexchange.com/questions/389675/largest-prime-factor-of-600851475143
 
+def is_prime(n):
+
+	if (str(n)[-1] == '2'):
+		return False;
+
+	top = int(floor(sqrt(n)))
+	div = 3
+	while div <= top:
+
+		if (n % div == 0):
+			return False
+
+		div += 2;
+
+		if (str(div)[-1] == '5'):
+			div += 2;
+
+	return True
+
 primes = [2,3,5,7];
 n = 2*1000*1000;
-n = 2*1000;
+n = 2*1000*1000;
 i = 11;
 
 while i < n:
@@ -18,4 +37,4 @@ while i < n:
 	if (str(i)[-1] == '5'):
 			i += 2;
 
-print 'The sum of primes under {n} is {sum(primes)}'
+print 'The sum of primes under {0} is {1}: {2}'.format(n, sum(primes), primes)
---
+++ src/main/python/com/thiagoh/projecteuler/Q4.py
@@ -0,0 +1,19 @@
+# the number divided by 1 to 10 can be achieved multiplying its factors
+1*2*3*4*5*6*7*8*9*10=3628800
+# but inside this factor there are some repetitions, like 2 and 5 and 10 (2*5=10), so we can remove them and last the following factors
+5*7*8*9=2520
+
+# from 1 to 20 goes like this
+19=ok
+18=2*9
+17=ok
+16=2*8
+15=3*5
+14=2*7
+13=ok
+12=2*6
+11=ok
+10=2*5
+9=3*3
+5*7*8*9*11*13*17*19=116396280
+5*7*9*11*13*16*17*19=232792560
