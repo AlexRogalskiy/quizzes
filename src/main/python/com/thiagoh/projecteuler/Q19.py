bdd1c80c69fa550db02d882e7a8705b691740620
Thiago Andrade <thiagoh@gmail.com>
2016-09-05 22:37:52 -0400

    :sparkles: question 19

--- src/main/python/com/thiagoh/projecteuler/Q19.py
+++ src/main/python/com/thiagoh/projecteuler/Q19.py
@@ -10,7 +10,39 @@ from math import *;
 # March is (31 + x) mod 7 = y => y = 0 <=== this is a sunday on the first day of month!
 # April is (30 + y) mod 7 = t => t = 2 ... go on with the search
 
-def solve(from, to):
-	pass
+months = [0,31,28,31,30,31,30,31,31,30,31,30,31];
 
-print 'The maximum total from top to bottom is: ' + str(solve(triangle_arr));
+def is_leap_year( year):
+	return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0;
+
+def get_days_of_month(month, year):
+	if month == 2 and is_leap_year(year): 
+		return 29;
+	return months[month];
+
+def solve(distance_from_sunday_in_days, from_month, from_year, to_month, to_year):
+
+	sunday_count = 0;
+
+	if distance_from_sunday_in_days == 0:
+		sunday_count += 1;
+
+	while from_year < to_year or (from_year == to_year and from_month < to_month):
+
+		days_of_month = get_days_of_month(from_month, from_year);
+		distance_from_sunday_in_days = (days_of_month + distance_from_sunday_in_days) % 7;
+
+		if (distance_from_sunday_in_days == 0):
+			sunday_count += 1;
+
+		from_month += 1;
+
+		if from_month == 13:
+			from_year += 1;
+			from_month = 1;
+
+		print 'from_year', from_year, 'from_month', from_month, 'distance_from_sunday_in_days', distance_from_sunday_in_days
+
+	return sunday_count
+
+print 'The number of Sundays is: ' + str(solve(2, 1, 1901, 12, 2000));
