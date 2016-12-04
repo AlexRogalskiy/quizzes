f92d1796ca7a4f3ed8afddb92c43a56352ad25f0
Thiago Andrade <thiagoh@gmail.com>
2016-12-04 18:12:11 -0500

    new tests for Q4_1

--- java/src/test/java/com/thiagoh/cci/c4/Q4_1Test.java
+++ java/src/test/java/com/thiagoh/cci/c4/Q4_1Test.java
@@ -32,7 +32,16 @@ public class Q4_1Test {
 		Node node11 = new Node(11);
 		Node node12 = new Node(12);
 		Node node13 = new Node(13);
-		
+		Node node14 = new Node(14);
+		Node node15 = new Node(15);
+		Node node16 = new Node(16);
+
+		node10.children = new Node[] { node11, node12 };
+		node11.children = new Node[] { node12, node13, node16 };
+		node13.children = new Node[] { node14 };
+		node14.children = new Node[] { node15 };
+		node16.children = new Node[] { node15 };
+
 		Assert.assertTrue(Q4_1.hasPath(node1, node2));
 		Assert.assertTrue(Q4_1.hasPath(node1, node3));
 		Assert.assertTrue(Q4_1.hasPath(node1, node4));
@@ -42,9 +51,30 @@ public class Q4_1Test {
 		Assert.assertTrue(Q4_1.hasPath(node1, node8));
 		Assert.assertTrue(Q4_1.hasPath(node1, node9));
 		
+		Assert.assertTrue(Q4_1.hasPath(node3, node5));
+		Assert.assertTrue(Q4_1.hasPath(node3, node9));
+		Assert.assertTrue(Q4_1.hasPath(node3, node7));
+		Assert.assertTrue(Q4_1.hasPath(node3, node8));
+		
+		Assert.assertTrue(Q4_1.hasPath(node4, node3));
+ 		Assert.assertTrue(Q4_1.hasPath(node4, node5));
+		Assert.assertTrue(Q4_1.hasPath(node4, node9));
+		Assert.assertTrue(Q4_1.hasPath(node4, node7));
+		Assert.assertTrue(Q4_1.hasPath(node4, node8));
+
 		Assert.assertFalse(Q4_1.hasPath(node1, node10));
 		Assert.assertFalse(Q4_1.hasPath(node1, node11));
 		Assert.assertFalse(Q4_1.hasPath(node1, node12));
 		Assert.assertFalse(Q4_1.hasPath(node1, node13));
+		
+		Assert.assertTrue(Q4_1.hasPath(node10, node11));
+		Assert.assertTrue(Q4_1.hasPath(node10, node12));
+		Assert.assertTrue(Q4_1.hasPath(node10, node13));
+		Assert.assertTrue(Q4_1.hasPath(node10, node16));
+		Assert.assertTrue(Q4_1.hasPath(node10, node15));
+		
+		Assert.assertTrue(Q4_1.hasPath(node13, node14));
+		Assert.assertTrue(Q4_1.hasPath(node13, node15));
+		Assert.assertFalse(Q4_1.hasPath(node13, node16));
 	}
 }
